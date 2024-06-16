17a94874a479d92b5f874d8e36aec553c92ed8a7
tauficls <tauficleonardo@gmail.com>
2024-06-16 20:17:21 +0700

    adding crud functions

--- notes/views.py
+++ notes/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.http import Http404
-from django.views.generic import CreateView, DetailView, ListView
-
+from django.views.generic import CreateView, UpdateView, DetailView, ListView
+from django.views.generic.edit import DeleteView
 
 from .models import Notes
 from .forms import NotesForm
+
+class NotesDeleteView(DeleteView):
+    model = Notes
+    success_url = '/smart/notes'
+    template_name = "notes/notes_delete.html"
+
+class NotesUpdateView(UpdateView):
+    model = Notes
+    success_url = '/smart/notes'
+    form_class = NotesForm
 
 class NotesCreateView(CreateView):
     model = Notes

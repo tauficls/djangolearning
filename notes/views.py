a79869986ad6cd4bebfdacd927921f7953d29b48
tauficls <tauficleonardo@gmail.com>
2024-06-16 16:51:37 +0700

    adding create form

--- notes/views.py
+++ notes/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from django.http import Http404
-from django.views.generic import DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView
 
-# Create your views here.
+
 from .models import Notes
+from .forms import NotesForm
+
+class NotesCreateView(CreateView):
+    model = Notes
+    # fields = ['title', 'text']
+    success_url = '/smart/notes'
+    form_class = NotesForm
 
 class NotesListView(ListView):
     model = Notes
